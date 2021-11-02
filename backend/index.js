b3057e50f094fb095834cf409b7648b6a392413a
sayyedmaroof <sayyedmaroof9@gmail.com>
2021-11-02 17:41:23 +0530

    setup for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -14,6 +15,15 @@ connectDB()
 app.use(express.json())
 app.use('/api/users', userRoutes)
 app.use('/api/notes', notesRoutes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 // error handling
 app.use(notFound)
